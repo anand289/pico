99b264529e5aab2c6a01eb793d82c2e39522873d
Anand Singh <aanandsingh289@gmail.com>
2023-01-21 18:58:35 -0800

    adding details.pages

--- motor.py
+++ motor.py
@@ -9,35 +9,46 @@ mtr_AI1 = machine.Pin(14,machine.Pin.OUT)
 mtr_AI2 = machine.Pin(15,machine.Pin.OUT)
 mtr_PWM  = machine.PWM(machine.Pin(16))
 
-mtr_AI1.value(0)
-mtr_AI2.value(1)
 mtr_PWM.freq(50)
 
 
 
 while True:
-    speed = 5
-    mtr_PWM.duty_u16(int(speed/100*65536))
-    utime.sleep(5)
-    
-    speed = 10
-    mtr_PWM.duty_u16(int(speed/100*65536))
-    utime.sleep(5)
-    
-    
-    speed = 15
-    mtr_PWM.duty_u16(int(speed/100*65536))
-    utime.sleep(5)
-    
-    
-    speed = 50
-    mtr_PWM.duty_u16(int(speed/100*65536))
-    utime.sleep(5)
     
     speed = 0
     mtr_PWM.duty_u16(int(speed/100*65536))
     utime.sleep(5)
     
-    break
+    # Direction 1
+    mtr_AI1.value(0)
+    mtr_AI2.value(1)
     
+    speed = 100
+    mtr_PWM.duty_u16(int(speed/100*65536))
+    utime.sleep(5)
+    
+    speed = 50
+    mtr_PWM.duty_u16(int(speed/100*65536))
+    utime.sleep(5)
+    
+    
+    speed = 0
+    mtr_PWM.duty_u16(int(speed/100*65536))
+    utime.sleep(2)
+    
+    
+    
+    # Direction 2
+    mtr_AI1.value(1)
+    mtr_AI2.value(0)
+    
+    speed = 100
+    mtr_PWM.duty_u16(int(speed/100*65536))
+    utime.sleep(5)
+    
+    speed = 50
+    mtr_PWM.duty_u16(int(speed/100*65536))
+    utime.sleep(5)
+    
+
     
